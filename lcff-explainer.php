42aa68b1d5ca5f91e4c61bd0144dae8a09a80526
bayreporta <bayreporta@gmail.com>
2015-10-26 16:29:46 -0700

    dynamic slides

--- lcff-explainer.php
+++ lcff-explainer.php
@@ -6,7 +6,8 @@
 		/* CUSTOM FIELDS
 		=================================================*/
 		$navigation = get_field('lcff_navigation', 89272);
-		//var_dump($navigation);
+		$content = get_field('lcff_slides', 89272);
+		//var_dump($content);
 
 		/* META TAGS
 		=================================================*/
@@ -84,7 +85,7 @@
 					$ret .= '</div>';
 
 
-					// NAVIGATION //
+					// TOP NAVIGATION //
 					$ret .= '<div id="lcff-top">';
 						$ret .= '<div>';
 							$ret .= '<a href="#" type="0" onclick="return false;"><i class="fa fa-chevron-left"></i> previous</a>';
@@ -95,43 +96,12 @@
 						$ret .= '</div>';
 					$ret .= '</div>';
 
+					// POPULATE SLIDES //
 					$ret .= '<aside class="lcff-content">';
-
-						// WELCOME SLIDE //
-						$ret .= '<div class="lcff-slide" type="0">';	
-							$ret .= '<img src="http://edsource.org/wp-content/uploads/2015/10/Growth-mindset-Garden-Grove.jpg">';	
-							$ret .= '<p>Welcome to an updated guide to the most sweeping changes in a half-century involving how California schools are governed and funded: the Local Control Funding Formula. The guide will take a look at the law two-plus years in to its implementation and will offer:</p>';
-							$ret .= '<p>The intent of the guide is not to judge whether the law has achieved Gov. Jerry Brown’s and the Legislature’s expectations. It is still early in the rollout and premature to make that determination. Instead, we hope that our information on the law will help readers take stock of  progress of their own districts in making the most of the opportunity that the law creates.</p>';
-							$ret .= '<p><em>We welcome your comments and suggestions on the section. Send them to <a href="mailto:edsource@edsource.org">edsource@edsource@org</a>.</em></p>';
-							$ret .= '<ul>';
-								$ret .= '<li>The intent of the guide is not to judge whether the law has achieved Gov. Jerry Brown’s and the Legislature’s expectations.</li>';
-								$ret .= '<li>The intent of the guide is not to judge whether the law has achieved Gov. Jerry Brown’s and the Legislature’s expectations.</li>';
-								$ret .= '<li>The intent of the guide is not to judge whether the law has achieved Gov. Jerry Brown’s and the Legislature’s expectations.</li>';
-								$ret .= '<li>The intent of the guide is not to judge whether the law has achieved Gov. Jerry Brown’s and the Legislature’s expectations.</li>';
-								$ret .= '<li>The intent of the guide is not to judge whether the law has achieved Gov. Jerry Brown’s and the Legislature’s expectations.</li>';
-							$ret .= '</ul>';
-							$ret .= '<p>The stakes are high. Whether the law delivers on its promise of improved academic outcomes for students will depend, in part, on the extent to which parents, teachers and community members guide local school boards and superintendents to set ambitious goals and make wise spending decisions. Thus, it is essential that Californians interested in improving education for the state’s children understand the new system and their responsibilities for contributing to its success.</p>';
-							$ret .= '<p>The stakes are high. Whether the law delivers on its promise of improved academic outcomes for students will depend, in part, on the extent to which parents, teachers and community members guide local school boards and superintendents to set ambitious goals and make wise spending decisions. Thus, it is essential that Californians interested in improving education for the state’s children understand the new system and their responsibilities for contributing to its success.</p>';
-
-						$ret .= '</div>';
-
-						// INTRO SLIDE //
-						$ret .= '<div class="lcff-slide" type="1">';		
-							$ret .= '<p>The Local Control Funding Formula is an ambitious experiment in school finance and governance that Gov. Jerry Brown proposed in 2012 and  the Legislature passed in 2013. Brown optimistically and enthusiastically expressed his aspirations for the Local Control Funding Formula on signing what he called a “truly revolutionary”  law.</p>';
-							$ret .= '<p>The stakes are high. Whether the law delivers on its promise of improved academic outcomes for students will depend, in part, on the extent to which parents, teachers and community members guide local school boards and superintendents to set ambitious goals and make wise spending decisions. Thus, it is essential that Californians interested in improving education for the state’s children understand the new system and their responsibilities for contributing to its success.</p>';
-						$ret .= '</div>';
+						for($i=0 ; $i < sizeof($content) ; $i++){
+							$ret .= '<div class="lcff-slide" type="'.$i.'">';$ret .= $content[$i]['slide_contents'];$ret .= '</div>';
+						}		
 					$ret .= '</aside>';
-
-					
-						
-
-
-						//$ret .= '<p></p>';
-						//$ret .= '<p></p>';
-						//$ret .= '<p></p>';
-						//$ret .= '<p></p>';
-
-					
 				$ret .= '</div>';
 			$ret .=  '</section>';
 		$ret .=  '</div>';
