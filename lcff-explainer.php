5688fe3cd0c0df4e0eb959061b466dc009f1277c
bayreporta <bayreporta@gmail.com>
2015-10-26 16:18:18 -0700

    dynamic navigation gen

--- lcff-explainer.php
+++ lcff-explainer.php
@@ -3,6 +3,11 @@
 	function lcff_explainer($id=null){
 		$ret = '';
 
+		/* CUSTOM FIELDS
+		=================================================*/
+		$navigation = get_field('lcff_navigation', 89272);
+		//var_dump($navigation);
+
 		/* META TAGS
 		=================================================*/
 		$ret .= '<meta charset="utf-8"><title>Local Control Funding Formula Guide | EdSource</title></meta>';
@@ -45,36 +50,28 @@
 					$ret .= '</div>';
 					$ret .= '<div>';
 						$ret .= '<ul>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="0"></i><li><a href="#" onclick="return false;" type="0">Welcome to the Guide</a></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="1"></i><li><a href="#" onclick="return false;" type="1">Introduction</a></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="2"></i><li><a href="#" onclick="return false;" type="2">Part 1 - Funding Schools Equitably</a><ul class="lcff-subnav" subnav="0" type="2">';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="3"></i><li><a href="#" onclick="return false;" type="3">What are the three components of the formula? How do they work?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="4"></i><li><a href="#" onclick="return false;" type="4">What does full funding mean?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="5"></i><li><a href="#" onclick="return false;" type="5">How far along are we?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="6"></i><li><a href="#" onclick="return false;" type="6">How will be the funding formula be phased in?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="7"></i><li><a href="#" onclick="return false;" type="7">Why does funding vary by district?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="8"></i><li><a href="#" onclick="return false;" type="8">How is money under the formula allocated?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="9"></i><li><a href="#" onclick="return false;" type="9">How must districts spend LCFF dollars?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="10"></i><li><a href="#" onclick="return false;" type="10">How should districts calculate yearly allocations?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="11"></i><li><a href="#" onclick="return false;" type="11">How restrictive are targeted dollars?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="12"></i><li><a href="#" onclick="return false;" type="12">How do small-class incentives work?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="13"></i><li><a href="#" onclick="return false;" type="13">What happened to transportation funding?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="14"></i><li><a href="#" onclick="return false;" type="14">What’s left out of the formula?</a></li>';
-							$ret .= '</ul></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="15"></i><li><a href="#" onclick="return false;" type="15"></i>Part 2 - Making Decisions Locally</a><ul class="lcff-subnav" subnav="1" type="15">';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="16"></i><li><a href="#" onclick="return false;" type="16">Introduction</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="17"></i><li><a href="#" onclick="return false;" type="17">What is an LCAP?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="18"></i><li><a href="#" onclick="return false;" type="18">What is in an LCAP?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="19"></i><li><a href="#" onclick="return false;" type="19">What are the LCAP’s eight priority areas?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="20"></i><li><a href="#" onclick="return false;" type="20">What is meaningful parent and student engagement?</a></li>';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="21"></i><li><a href="#" onclick="return false;" type="21">What is the role of the county office of education?</a></li>';
-							$ret .= '</ul></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="22"></i><li><a href="#" onclick="return false;" type="22">Part 3 - Measuring School Achivement Broadly</a><ul class="lcff-subnav" subnav="2" type="22">';
-								$ret .= '<i class="fa fa-arrow-circle-right" type="23"></i><li><a href="#" onclick="return false;" type="23">What will the California Collaborative for Educational Excellence do?</a></li>';
-							$ret .= '</ul></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="24"></i><li><a href="#" onclick="return false;" type="24">Charter Schools and the Funding Law</a></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="25"></i><li><a href="#" onclick="return false;" type="25">How\'s It Going So Far?</a></li>';
-							$ret .= '<i class="fa fa-arrow-circle-right" type="26"></i><li><a href="#" onclick="return false;" type="26">Conclusion</a></li>';
+
+							// SLIDE NAVIGATION AREA //
+							$slides = 0; $subnav = 0;
+							for ($i=0 ; $i < sizeof($navigation) ; $i++){
+								$ret .= '<i class="fa fa-arrow-circle-right" type="'.$slides.'"></i><li><a href="#" onclick="return false;" type="'.$slides.'">'.$navigation[$i]['slide_title'].'</a>';
+
+								// Add subnav if applicable //
+								if($navigation[$i]['haz_subnav']){
+									$ret .= '<ul class="lcff-subnav" subnav="'.$subnav.'" type="'.$slides.'">';
+
+									for ($j = 0 ; $j < sizeof($navigation[$i]['lcff_sub_navigation']) ; $j++){
+										$slides += 1;
+										$ret .= '<i class="fa fa-arrow-circle-right" type="'.$slides.'"></i><li><a href="#" onclick="return false;" type="'.$slides.'">'.$navigation[$i]['lcff_sub_navigation'][$j]['slide_title'].'</a></li>';
+									}
+
+									$ret .= '</ul></li>';
+									$subnav += 1;
+								}
+								else {$ret .= '</li>';}
+
+								$slides += 1;
+							}
 						$ret .= '</ul>';
 					$ret .= '</div>';
 
